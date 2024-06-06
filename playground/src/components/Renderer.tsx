6eee4eef22a46cefe66ea1fb31bbc9ca9fe175c2
yarnb <yarnb@foxmail.com>
2024-06-06 13:00:00 +0800

    chore

--- playground/src/components/Renderer.tsx
+++ playground/src/components/Renderer.tsx
@@ -4,21 +4,40 @@ import { useEventBus } from "../hooks/useEventBus";
 import { useEffect, useState } from "react";
 
 export const Renderer = () => {
-  const [text, setText] = useState("");
+  const [nodes, setNodes] = useState<React.ReactNode>();
+  const [error, setError] = useState<string | null>(null);
 
   const eventBus = useEventBus();
 
   useEffect(() => {
     return eventBus.on("editTextChange", (newText) => {
-      setText(newText);
+      try {
+        const nodes = dslToReactNodes(newText, components);
+        setNodes(nodes);
+        setError(null);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message); // 设置错误信息
+        } else {
+          setError(String(err));
+        }
+      }
     });
   }, []);
 
   return (
     <div
-      style={{ height: "100vh", width: "50%", borderLeft: "1px solid #ccc" }}
+      style={{
+        height: "100vh",
+        flexGrow: 1,
+        borderLeft: "1px solid #ccc",
+      }}
     >
-      {dslToReactNodes(text, components)}
+      {error ? (
+        <div style={{ color: "red" }}>Error rendering content: {error}</div>
+      ) : (
+        nodes
+      )}
     </div>
   );
 };
